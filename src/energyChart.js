325951250489013b4a27ae0acddb3f45f92c35b0
Neil Rayu <neilrayu@gmail.com>
2022-01-16 16:50:18 -0600

    init

---
+++ src/energyChart.js
@@ -0,0 +1,53 @@
+import Chart from "chart.js/auto";
+const ctx = document.getElementById("energy-chart").getContext("2d");
+
+const dayTimeForm = document.querySelector("[class='dayTimeForm']");
+const dayStart = document.querySelector("[id='startInput']");
+const dayEnd = document.querySelector("[id='endInput']");
+
+function calculateDay(start, end) {
+  for (let i = start; i <= end; i++) {
+    label.push(i);
+  }
+}
+
+const dayV = [];
+
+dayTimeForm.onsubmit = (e) => {
+  e.preventDefault();
+  //calculateDay(dayStart.value, dayEnd.value);
+  console.log(dayStart.value);
+  let hourLabel = [];
+  for (let i = parseInt(dayStart.value); i <= dayEnd.value; i++) {
+    hourLabel.push(i);
+  }
+  console.log(hourLabel);
+  const energyChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: hourLabel,
+      datasets: [
+        {
+          label: "Alertness",
+          data: [10, 9, 9, 10, 7, 4, 8, 7.5, 6, 5, 8, 8],
+          borderColor: "rgb(75, 192, 192)",
+        },
+        {
+          label: "Energy",
+          data: [10, 10, 9, 10, 7, 4, 8, 7, 6, 5, 9, 9],
+          borderColor: "rgb(195, 144, 25)",
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+
+  dayV.push(dayStart.value);
+  dayV.push(dayEnd.value);
+};
---
+++ src/index.js
@@ -0,0 +1,52 @@
+//import { startEnd } from "./energyChart";
+// doccument.getElementById("click").onclick = function hello() {
+//   document.getElementById("click").textContent = "hello";
+// };
+var date = new Date().toLocaleDateString("en-US", {
+  day: "numeric",
+  month: "short",
+  year: "numeric",
+});
+const dateHead = document.createElement("h3");
+dateHead.innerHTML = "Today - " + date;
+document.getElementById("heading").appendChild(dateHead);
+
+const form = document.querySelector("[class='taskForm']");
+const inputTask = document.querySelector("[id='task-input']");
+const todoList = document.getElementById("todos");
+
+const todoData = [];
+const timeData = [];
+const locData = [];
+
+function addTodo(todoText) {
+  todoData.push(todoText.split(",")[0]);
+  timeData.push(todoText.split(",")[1]);
+  locData.push(todoText.split(",")[2]);
+  const li = document.createElement("li");
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  li.appendChild(checkBox);
+  li.textContent =
+    "I will do " +
+    todoData.at(-1) +
+    " in my " +
+    locData.at(-1) +
+    " when the clock strikes " +
+    timeData.at(-1);
+  todoList.appendChild(li);
+}
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  addTodo(inputTask.value);
+};
+
+document.getElementById("add-task").onclick = function openInput() {
+  document.getElementById("inner").style.clipPath = "circle(75%)";
+};
+
+document.querySelector("[class='cancel-task']").onclick =
+  function closeInput() {
+    document.getElementById("inner").style.clipPath = "circle(0%)";
+  };
